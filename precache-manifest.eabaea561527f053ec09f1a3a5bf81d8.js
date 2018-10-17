a5a09292c5d24ff79766a74cc227656fca49623d
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2018-10-17 12:21:47 +0530

    Updates

---
+++ precache-manifest.eabaea561527f053ec09f1a3a5bf81d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7de484aaff74c1507f8a",
+    "url": "/random-quote-generator/static/js/runtime~main.7de484aa.js"
+  },
+  {
+    "revision": "0085c2d62072c739555e",
+    "url": "/random-quote-generator/static/js/main.0085c2d6.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/random-quote-generator/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "0085c2d62072c739555e",
+    "url": "/random-quote-generator/static/css/main.1103119f.chunk.css"
+  },
+  {
+    "revision": "8a9b51d682753440b923bfe9f25dd46f",
+    "url": "/random-quote-generator/index.html"
+  }
+];
